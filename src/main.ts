4ac40f860e201b03e78c418c1247e8909a8c6b20
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-07-14 13:45:45 +0800

    feat: tree shaking for element plus

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createPinia } from 'pinia'
 
-const app = createApp(App)
-    .use(ElementPlus)
-    .use(createPinia())
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-}
+const app = createApp(App).use(createPinia())
 app.mount('#app')
